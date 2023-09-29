191c2649799286d4b711560b88e7bc8b157ef1ac
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2023-09-29 21:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 //your JS code here. If required.
 // document.addEventListener('DOMContentLoaded', function () {
   const colorSelect = document.getElementById('colorSelect');
-  const removeButton = document.querySelector('input[type="button"]');
+  const removeButton = document.getElementsByTagName('input')[0];
 
-  removeButton.addEventListener('click', function () {
+
+  removeButton.addEventListener('click',  ()=> {
     const selectedOption = colorSelect.options[colorSelect.selectedIndex];
-
+    
+    
     if (selectedOption) {
       colorSelect.removeChild(selectedOption);
     }
   });
 // });
+
+
+
+
+
+
+
+
+
+
+
